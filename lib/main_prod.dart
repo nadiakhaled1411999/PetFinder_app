d1bb3083c1ec179fe7db24492e3ffdcc7d0339bd
Nadia khaled <d703403@gmail.com>
2025-12-28 22:48:42 +0200

    added development&productions flavors for android. changed android app name for each flavors . added launch.json for flavors config. added two main files for each flavor.

---
+++ lib/main_prod.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:petfinder_app/petfinder_app.dart';
+
+void main()async {
+ WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  runApp(const PetFinderApp());
+}
+
